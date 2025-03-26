66231a916427a9371dbeff6c7e58177a7d0f8c74
Natsuki Miyadi <mozc67@gmail.com>
2025-03-27 00:19:32 +0900

    Add IRSA and OIDC provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_rHCqR8mhF",
   client_id: "3tdd1ec5am5tci65s7tdkofpv4",
-  redirect_uri: "https://ap-northeast-1rhcqr8mhf.auth.ap-northeast-1.amazoncognito.com/oauth2/idpresponse",
+  redirect_uri: "https://todo-app.natsuki-cloud.dev.auth.ap-northeast-1.amazoncognito.com/oauth2/authorize?",
   response_type: "code",
   scope: "email openid profile",
 };
