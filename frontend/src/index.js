69a832513d7b28bb26483540f82e556cf37f5c6e
Natsuki Miyadi <mozc67@gmail.com>
2025-03-29 20:20:33 +0900

    feat: implement React routing for auth callback

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Root from "./App";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_rHCqR8mhF",
   client_id: "3tdd1ec5am5tci65s7tdkofpv4",
-  redirect_uri: "https://todo-app.natsuki-cloud.dev.auth.ap-northeast-1.amazoncognito.com/oauth2/authorize?",
+  redirect_uri: "https://todo-app.natsuki-cloud.dev/callback",
   response_type: "code",
   scope: "email openid profile",
 };
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider {...cognitoAuthConfig}>
-      <App />
+      <Root />
     </AuthProvider>
   </React.StrictMode>
 );
